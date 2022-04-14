cf874aa81a9facc0c8ec55886f8eefb379eee992
Ilyass Taouil <taouil@robo6.informatik.uni-bonn.de>
2022-04-14 14:39:44 +0200

    Updated bridge with correct fields.

--- src/aliengo_bridge.cpp
+++ src/aliengo_bridge.cpp
@@ -4,244 +4,188 @@ using namespace UNITREE_LEGGED_SDK;
 
 namespace aliengo_bridge
 {
+    AlienGoBridge::AlienGoBridge(ros::NodeHandle ph)
+    : m_ph( ph )
+    , m_safe(LeggedType::Aliengo)
+    , m_udp(8081, "192.168.123.220", 8082, sizeof(HighCmd), sizeof(HighState))
+    , m_loop_control("control_loop", m_dt, boost::bind(&AlienGoBridge::control, this))
+    , m_loop_udpSend("udp_send", m_dt, 3, boost::bind(&AlienGoBridge::UDPSend, this))
+    , m_loop_udpRecv("udp_recv", m_dt, 3, boost::bind(&AlienGoBridge::UDPRecv, this))
+    {
+        // ROS
+        m_cmd_sub = ph.subscribe( "cmd", 1, &AlienGoBridge::cmdCallback, this );
+        m_state_pub = ph.advertise< unitree_legged_msgs::HighState >( "high_state", 1 );
+        
+        // SDK
+        m_udp.InitCmdData(m_cmd);
+        m_motion_timestep = static_cast<int>( 1000 * m_dt );
+        
+        // Leave it commented
+        //InitEnvironment();
+        
+        m_loop_udpSend.start();
+        m_loop_udpRecv.start();
+        m_loop_control.start();
+    }
 
-AlienGoBridge::AlienGoBridge(ros::NodeHandle ph)
-: m_ph( ph )
-, m_safe(LeggedType::Aliengo)
-, m_udp(8090, "192.168.123.220", 8082, sizeof(HighCmd), sizeof(HighState))
-, m_loop_control("control_loop", m_dt, boost::bind(&AlienGoBridge::control, this))
-, m_loop_udpSend("udp_send", m_dt, 3, boost::bind(&AlienGoBridge::UDPSend, this))
-, m_loop_udpRecv("udp_recv", m_dt, 3, boost::bind(&AlienGoBridge::UDPRecv, this))
-{
-    // ROS
-    m_cmd_sub = ph.subscribe( "cmd", 1, &AlienGoBridge::cmdCallback, this );
-    m_state_pub = ph.advertise< unitree_legged_msgs::HighState >( "high_state", 1 );
-    
-    // SDK
-    m_udp.InitCmdData(m_cmd);
-    m_motion_timestep = static_cast<int>( 1000 * m_dt );
-    
-    // Leave it commented
-    //InitEnvironment();
-    
-    m_loop_udpSend.start();
-    m_loop_udpRecv.start();
-    m_loop_control.start();
-}
-
-void AlienGoBridge::cmdCallback( const unitree_legged_msgs::HighCmd& cmd )
-{
-    // boost::mutex::scoped_lock lock(m_cmd_mutex);
-    // m_cmd.mode = cmd.mode;      // 0:idle, default stand      1:forced stand     2:walk continuously
-    
-    // m_cmd.forwardSpeed = cmd.forwardSpeed;
-    // m_cmd.sideSpeed = cmd.sideSpeed;
-    // m_cmd.rotateSpeed = cmd.rotateSpeed;
-    // m_cmd.bodyHeight = cmd.bodyHeight;
-    
-    // m_cmd.yaw = cmd.yaw;
-    // m_cmd.roll  = cmd.roll;
-    // m_cmd.pitch = cmd.pitch;
-    
-    // m_received_cmd = true;
-    // m_last_cmd_time = ros::Time::now();
-}
-
-void AlienGoBridge::UDPRecv()
-{
-    m_udp.Recv();
-}
-
-void AlienGoBridge::UDPSend()
-{  
-    m_udp.Send();
-}
-
-void AlienGoBridge::resetCmd()
-{
-    m_cmd.mode = 0;               // 0. idle, default stand
-                                // 1. force stand (controlled by dBodyHeight + ypr)
-                                // 2. target velocity walking (controlled by velocity + yawSpeed)
-                                // 3. target position walking (controlled by position + ypr[0])
+    void AlienGoBridge::cmdCallback( const unitree_legged_msgs::HighCmd& cmd )
+    {
+        boost::mutex::scoped_lock lock(m_cmd_mutex);
+        m_cmd.mode = cmd.mode;    // 0.idle, default stand | 1.force stand (controlled by dBodyHeight + rpy)
+                                // 2.target velocity walking (controlled by velocity + yawSpeed)
+                                // 3.target position walking (controlled by position + rpy[2])
                                 // 4. path mode walking (reserve for future release)
-                                // 5. position stand down. 
-                                // 6. position stand up 
-                                // 7. damping mode
-                                // 8. recovery stand
-                                // 9. backflip
-                                // 10. jumpYaw
-                                // 11. straightHand
-                                // 12. dance1
-                                // 13. dance2
-    m_cmd.gaitType = 0;           // 0.idle  1.trot  2.trot running  3.climb stair
-    m_cmd.euler[0] = 0;           // (unit: rad), roll in stand mode
-    m_cmd.euler[1] = 0;           // (unit: rad), pitch in stand mode
-    m_cmd.euler[2] = 0;           // (unit: rad), yaw in stand mode
-    m_cmd.speedLevel = 0;         // 0. default low speed. 1. medium speed 2. high speed. during walking, only respond MODE 3
-    m_cmd.bodyHeight = 0;         // (unit: m, default: 0.28m)
-    m_cmd.yawSpeed = 0.0f;        // (unit: rad/s), rotateSpeed in body frame
-    m_cmd.velocity[0] = 0.0f;     // (unit: m/s), forwardSpeed in body frame
-    m_cmd.velocity[1] = 0.0f;     // (unit: m/s), sideSpeed in body frame
-    m_cmd.footRaiseHeight = 0;    // (unit: m, default: 0.08m), foot up height while walking
-}
-
-void AlienGoBridge::control() 
-{
-    m_state_mutex.lock();
-    m_udp.GetRecv( m_state );
-    m_state_mutex.unlock();
-
-    // memcpy(&_keyData, m_state.wirelessRemote, 40);
-
-    // if ((ros::Time::now() - m_last_joy_time).toSec() > 1) 
-    // {
-    //     // if((int)_keyData.btn.components.select == 1)
-    //     // {
-    //     //     m_cmd_max_velocity = 0.1;
-    //     //     m_cmd_min_velocity = 0.0; 
-
-    //     //     m_last_joy_time = ros::Time::now();
-    //     //     std::cout << "Reset max and min velocities to: " << 0.1 << " and " << 0.0 << std::endl;
-    //     // }
-    //     if((int)_keyData.btn.components.Y == 1)
-    //     {
-    //         if (m_cmd_max_velocity < 0.9)
-    //             m_cmd_max_velocity += 0.1;
-            
-    //         m_last_joy_time = ros::Time::now();
-    //         std::cout << "Current max velocity is: " << m_cmd_max_velocity << std::endl;
-    //     }
-    //     else if((int)_keyData.btn.components.A == 1)
-    //     {
-    //         if (m_cmd_max_velocity > 0.0)
-    //             m_cmd_max_velocity -= 0.1;
-
-    //         m_last_joy_time = ros::Time::now();
-    //         std::cout << "Current max velocity is: " << m_cmd_max_velocity << std::endl;
-    //     }
-    //     else if((int)_keyData.btn.components.B == 1)
-    //     {
-    //         if (m_cmd_min_velocity < 0.8)
-    //             m_cmd_min_velocity += 0.1;
-            
-    //         m_last_joy_time = ros::Time::now();
-    //         std::cout << "Current min velocity is: " << m_cmd_min_velocity << std::endl;
-    //     }
-    //     else if((int)_keyData.btn.components.X == 1)
-    //     {
-    //         if (m_cmd_min_velocity > 0.0)
-    //             m_cmd_min_velocity -= 0.1;
-            
-    //         m_last_joy_time = ros::Time::now();
-    //         std::cout << "Current min velocity is: " << m_cmd_min_velocity << std::endl;
-    //     }
-    // }
-
-    // if (_keyData.ly > 0.95)
-    // {
-    //     std::cout << "Sending fwd max velocity command: " <<  << m_cmd_max_velocity << std::endl;
-    // }
-    // else if (_keyData.ly < -0.95)
-    // {
-    //     std::cout << "Sending bwd max velocity command: " << -m_cmd_min_velocity << std::endl;
-    // }
-    // else if (_keyData.lx > 0.95)
-    // {
-    //     std::cout << "Sending clockwise velocity command: " << m_cmd_max_velocity << std::endl;
-    // }
-    // else if (_keyData.lx < -0.95)
-    // {
-    //     std::cout << "Sending counter clockwise velocity command: " << -m_cmd_max_velocity << std::endl;
-    // }
-    // else
-    // {
-    //     std::cout << "Sending fwd min velocity command: " << m_cmd_min_velocity << std::endl;
-    // }
-    
-    m_cmd_mutex.lock();
-    if (!m_received_cmd)
-    {
-        ROS_WARN_STREAM_THROTTLE( 5, "No cmd received." );
-        resetCmd();
-    }
-    // reset cmd after timeout
-    else if ((ros::Time::now() - m_last_cmd_time).toSec() > m_cmd_timeout)
-    {
-        ROS_WARN_STREAM( "Cmd timed out." );
-        resetCmd();
-    }
-    
-    m_udp.SetSend(m_cmd);
-    m_cmd_mutex.unlock();
-    
-    publishState();
-}
-
-void AlienGoBridge::publishState()
-{
-    boost::mutex::scoped_lock lock(m_state_mutex);
-    
-    // Messages to be published
-    sensor_msgs::Imu l_imuMsg;
-    unitree_legged_msgs::HighState msg;
-    sensor_msgs::JointState l_jointStateMsg;
-    
-    msg.levelFlag = m_state.levelFlag;
-    msg.commVersion = m_state.commVersion;
-    msg.robotID = m_state.robotID;
-    msg.SN = m_state.SN;
-    msg.bandWidth = m_state.bandWidth;
-    msg.mode = m_state.mode;
-    msg.progress = m_state.progress;
-    
-    msg.imu.quaternion[0] = m_state.imu.quaternion[0];
-    msg.imu.quaternion[1] = m_state.imu.quaternion[1];
-    msg.imu.quaternion[2] = m_state.imu.quaternion[2];
-    msg.imu.quaternion[3] = m_state.imu.quaternion[3];
-    msg.imu.gyroscope[0] = m_state.imu.gyroscope[0];
-    msg.imu.gyroscope[1] = m_state.imu.gyroscope[1];
-    msg.imu.gyroscope[2] = m_state.imu.gyroscope[2];
-    msg.imu.accelerometer[0] = m_state.imu.accelerometer[0];
-    msg.imu.accelerometer[1] = m_state.imu.accelerometer[1];
-    msg.imu.accelerometer[2] = m_state.imu.accelerometer[2];
-    msg.imu.temperature = m_state.imu.temperature;
-    
-    msg.gaitType = m_state.gaitType;
-    msg.footRaiseHeight = m_state.footRaiseHeight;
-
-    msg.position[0] = m_state.position[0];
-    msg.position[1] = m_state.position[1];
-    msg.position[2] = m_state.position[2];
-    
-    msg.bodyHeight = m_state.bodyHeight;
-    
-    msg.velocity[0] = m_state.velocity[0];
-    msg.velocity[1] = m_state.velocity[1];
-    msg.velocity[2] = m_state.velocity[2];
-    
-    msg.yawSpeed = m_state.yawSpeed;
-    
-    for ( int i = 0; i < 4; ++i )
-    {
-        msg.footPosition2Body[i].x = m_state.footPosition2Body[i].x;
-        msg.footPosition2Body[i].y = m_state.footPosition2Body[i].y;
-        msg.footPosition2Body[i].z = m_state.footPosition2Body[i].z;
+                                // 5. position stand down. |6. position stand up |7. damping mode | 8. recovery mode
         
-        msg.footSpeed2Body[i].x = m_state.footSpeed2Body[i].x;
-        msg.footSpeed2Body[i].y = m_state.footSpeed2Body[i].y;
-        msg.footSpeed2Body[i].z = m_state.footSpeed2Body[i].z;
+        m_cmd.gaitType = cmd.gaitType; // 0.trot | 1. trot running  | 2.climb stair
+
+        m_cmd.speedLevel = cmd.speedLevel; // 0. default low speed. 1. medium speed 2. high speed. during walking
         
-        msg.footForce[i] = m_state.footForce[i];
-        msg.footForceEst[i] = m_state.footForceEst[i];
+        m_cmd.dFootRaiseHeight = cmd.dFootRaiseHeight; // (unit: m), swing foot height adjustment from default swing height.
+
+        m_cmd.dBodyHeight = cmd.dBodyHeight; // (unit: m), body height adjustment from default body height.
+
+        m_cmd.position[0] = cmd.position[0]; // (unit: m), desired x in inertial frame.
+        m_cmd.position[1] = cmd.position[1]; // (unit: m), y position in inertial frame.
+
+        m_cmd.rpy[0] = cmd.rpy[0]; // (unit: rad), desired roll euler angle
+        m_cmd.rpy[1] = cmd.rpy[1]; // (unit: rad), desired pitch euler angle
+        m_cmd.rpy[2] = cmd.rpy[2]; // (unit: rad), desired yaw euler angle
+
+        m_cmd.velocity[0] = cmd.velocity[0]; // (unit: m/s), forwardSpeed in body frame.
+        m_cmd.velocity[1] = cmd.velocity[1]; // (unit: m/s), sideSpeed in body frame.
+        m_cmd.yawSpeed = cmd.yawSpeed;    // (unit: rad/s), rotateSpeed in body frame.
+
+        m_received_cmd = true;
+        m_last_cmd_time = ros::Time::now();
     }
-    
-    for ( int i = 0; i < 40; ++i )
-        msg.wirelessRemote[i] = m_state.wirelessRemote[i];
 
-    msg.reserve = m_state.reserve;
-    msg.crc = m_state.crc;
-    
-    m_state_pub.publish( msg );
-}
+    void AlienGoBridge::UDPRecv()
+    {
+        m_udp.Recv();
+    }
 
+    void AlienGoBridge::UDPSend()
+    {  
+        m_udp.Send();
+    }
+
+    void AlienGoBridge::resetCmd()
+    {
+        m_cmd.mode = 0;           // 0.idle, default stand | 1.force stand (controlled by dBodyHeight + rpy)
+                                // 2.target velocity walking (controlled by velocity + yawSpeed)
+                                // 3.target position walking (controlled by position + rpy[2])
+                                // 4. path mode walking (reserve for future release)
+                                // 5. position stand down. |6. position stand up |7. damping mode | 8. recovery mode
+        
+        m_cmd.gaitType = 2; // 0.trot | 1. trot running  | 2.climb stair
+
+        m_cmd.speedLevel = 0; // 0. default low speed. 1. medium speed 2. high speed. during walking
+        
+        m_cmd.dFootRaiseHeight = 0.0f; // (unit: m), swing foot height adjustment from default swing height.
+
+        m_cmd.dBodyHeight = 0.0f; // (unit: m), body height adjustment from default body height.
+
+        m_cmd.position[0] = 0.0f; // (unit: m), desired x in inertial frame.
+        m_cmd.position[1] = 0.0f; // (unit: m), y position in inertial frame.
+
+        m_cmd.rpy[0] = 0.0f; // (unit: rad), desired roll euler angle
+        m_cmd.rpy[1] = 0.0f; // (unit: rad), desired pitch euler angle
+        m_cmd.rpy[2] = 0.0f; // (unit: rad), desired yaw euler angle
+
+        m_cmd.velocity[0] = 0.0f; // (unit: m/s), forwardSpeed in body frame.
+        m_cmd.velocity[1] = 0.0f; // (unit: m/s), sideSpeed in body frame.
+        m_cmd.yawSpeed = 0.0f;    // (unit: rad/s), rotateSpeed in body frame.
+    }
+
+    void AlienGoBridge::control() 
+    {
+        m_state_mutex.lock();
+        m_udp.GetRecv( m_state );
+        m_state_mutex.unlock();
+        
+        m_cmd_mutex.lock();
+        if (!m_received_cmd)
+        {
+            ROS_WARN_STREAM_THROTTLE( 5, "No cmd received." );
+            resetCmd();
+        }
+        // reset cmd after timeout
+        else if ((ros::Time::now() - m_last_cmd_time).toSec() > m_cmd_timeout)
+        {
+            ROS_WARN_STREAM( "Cmd timed out." );
+            resetCmd();
+        }
+        
+        m_udp.SetSend(m_cmd);
+        m_cmd_mutex.unlock();
+        
+        publishState();
+    }
+
+    void AlienGoBridge::publishState()
+    {
+        boost::mutex::scoped_lock lock(m_state_mutex);
+        
+        sensor_msgs::Imu l_imuMsg;
+        unitree_legged_msgs::HighState msg;
+        sensor_msgs::JointState l_jointStateMsg;
+        
+        msg.levelFlag = m_state.levelFlag;
+        msg.commVersion = m_state.commVersion;
+        msg.robotID = m_state.robotID;
+        msg.SN = m_state.SN;
+        msg.bandWidth = m_state.bandWidth;
+        msg.mode = m_state.mode;
+        
+        msg.imu.quaternion[0] = m_state.imu.quaternion[0];
+        msg.imu.quaternion[1] = m_state.imu.quaternion[1];
+        msg.imu.quaternion[2] = m_state.imu.quaternion[2];
+        msg.imu.quaternion[3] = m_state.imu.quaternion[3];
+        msg.imu.gyroscope[0] = m_state.imu.gyroscope[0];
+        msg.imu.gyroscope[1] = m_state.imu.gyroscope[1];
+        msg.imu.gyroscope[2] = m_state.imu.gyroscope[2];
+        msg.imu.accelerometer[0] = m_state.imu.accelerometer[0];
+        msg.imu.accelerometer[1] = m_state.imu.accelerometer[1];
+        msg.imu.accelerometer[2] = m_state.imu.accelerometer[2];
+        msg.imu.rpy[0] = m_state.imu.rpy[0];
+        msg.imu.rpy[1] = m_state.imu.rpy[1];
+        msg.imu.rpy[2] = m_state.imu.rpy[2];
+        msg.imu.temperature = m_state.imu.temperature;
+        
+        msg.gaitType = m_state.gaitType;
+        msg.footRaiseHeight = m_state.footRaiseHeight;
+        msg.bodyHeight = m_state.bodyHeight;
+
+        msg.position[0] = m_state.position[0];
+        msg.position[1] = m_state.position[1];
+        msg.position[2] = m_state.position[2];
+        
+        msg.velocity[0] = m_state.velocity[0];
+        msg.velocity[1] = m_state.velocity[1];
+        msg.velocity[2] = m_state.velocity[2];
+        
+        msg.yawSpeed = m_state.yawSpeed;
+        
+        for ( int i = 0; i < 4; ++i )
+        {
+            msg.footPosition2Body[i].x = m_state.footPosition2Body[i].x;
+            msg.footPosition2Body[i].y = m_state.footPosition2Body[i].y;
+            msg.footPosition2Body[i].z = m_state.footPosition2Body[i].z;
+            
+            msg.footSpeed2Body[i].x = m_state.footSpeed2Body[i].x;
+            msg.footSpeed2Body[i].y = m_state.footSpeed2Body[i].y;
+            msg.footSpeed2Body[i].z = m_state.footSpeed2Body[i].z;
+            
+            msg.footForce[i] = m_state.footForce[i];
+        }
+        
+        for ( int i = 0; i < 40; ++i )
+            msg.wirelessRemote[i] = m_state.wirelessRemote[i];
+
+        msg.reserve = m_state.reserve;
+        msg.crc = m_state.crc;
+        
+        m_state_pub.publish( msg );
+    }
 }
