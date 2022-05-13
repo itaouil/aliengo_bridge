d226cf35afeaf0db08192ab24a629f7d005f553c
Ilyass Taouil <taouil@robo6.informatik.uni-bonn.de>
2022-05-13 12:44:22 +0200

    Final version for tf sensors script.

--- scripts/tf_sensors.py
+++ scripts/tf_sensors.py
@@ -11,17 +11,15 @@ def odom_callback(msg):
     br = tf.TransformBroadcaster()
 
     # T265 rot and translation
-    t265_world_rot = tf.transformations.quaternion_matrix([0, 0, 0, 0])
-    t265_world_trans = tf.transformations.translation_matrix([msg.pose.pose.position.x,
-                                                              msg.pose.pose.position.y,
-                                                              msg.pose.pose.position.z])
+    t265_world_rot = tf.transformations.quaternion_matrix([0, -0.1218693, 0, 0.9925462])
+    t265_world_trans = tf.transformations.translation_matrix([-0.33118, 0, -0.0045])
 
     # Publish static transform for t265 pose
     br.sendTransform(tf.transformations.translation_from_matrix(t265_world_trans),
                      tf.transformations.quaternion_from_matrix(t265_world_rot),
                      rospy.Time.now(),
-                     "t265_pose_frame",
-                     "t265_link")
+                     "base",
+                     "t265_pose_frame")
 
 def main():
     # Create ROS node
@@ -36,3 +34,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
